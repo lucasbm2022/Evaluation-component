c38a5387a5fa8855ad0d9eaf437267477b7b4c86
Lucas Bezerra Matias <100497621+lucasbm2022@users.noreply.github.com>
2022-09-27 16:07:54 -0300

    Commit

---
+++ cartão-avaliação/index.js
@@ -0,0 +1,24 @@
+function avaliar(){
+  alert("Thanks for the review!")
+}
+
+document.getElementById("n1").addEventListener("click", function changeColor1() {
+  n1.style.background = "hsl(216, 12%, 54%)";
+})
+
+document.getElementById("n2").addEventListener("click", function changeColor2() {
+  n2.style.background = "hsl(216, 12%, 54%)";
+})
+
+document.getElementById("n3").addEventListener("click", function changeColor3() {
+  n3.style.background = "hsl(216, 12%, 54%)";
+})
+
+document.getElementById("n4").addEventListener("click", function changeColor4() {
+  n4.style.background = "hsl(216, 12%, 54%)";
+})
+
+document.getElementById("n5").addEventListener("click", function changeColor5() {
+  n5.style.background = "hsl(216, 12%, 54%)";
+})
+
